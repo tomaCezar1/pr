20df24fa32f9f8a9a0a16965177fd18255b272ad
Cezar <cezar.toma1@gmail.com>
2022-05-19 12:32:38 +0300

    update config for email

--- Lab2/config.js
+++ Lab2/config.js
@@ -1,7 +1,7 @@
 module.exports.SEND_MAIL_CONFIG = {
   service: 'hotmail',
   auth: {
-    user: 'node.js.test.acc1@hotmail.com',
+    user: 'nodejs.test.test1@hotmail.com',
     pass: 'abc123456789',
   },
 };
